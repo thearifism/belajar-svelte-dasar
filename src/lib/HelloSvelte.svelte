0a739d0401a013b53933f7530fcd4f2c8c7dc7c5
krearive <arifblogger77@gmail.com>
2025-05-01 11:14:57 +0700

    Add new "Hello Svelte" example with dedicated entry point
    Introduce a "Hello Svelte" demo including a new HTML and Svelte component. Updated `vite.config.js` to support multiple entry points for building the project. This enhances the project with an additional template for Svelte learning or testing.

---
+++ src/lib/HelloSvelte.svelte
@@ -0,0 +1,11 @@
+<script>
+  console.log("Hello Svelte");
+</script>
+
+<h1>Hello Svelte!</h1>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        hello: 'hello.html',
+      },
+    },
+  },
 })
