aa6de2ca175fcf64a3752c1cf40f5f21699ab614
krearive <arifblogger77@gmail.com>
2025-05-01 12:01:05 +0700

    Refactor Counter to support array-based counting.
    Changed the `count` variable from a scalar to an array to track multiple increments. Introduced a derived `total` to calculate the sum dynamically, ensuring accurate display of the sum and individual increments. Updated the UI to reflect the new format.

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,11 +1,19 @@
 <script>
-  let count = $state(0);
+  let count = $state([]);
+  // let total = $derived(count.reduce((a, b) => a + b, 0));
+  let total = $derived.by(() => {
+    let total = 0;
+    for (let i = 0; i < count.length; i++) {
+      total += count[i];
+    }
+    return total;
+  });
 
   function increment() {
-    count++;
+    count.push(1);
   }
 </script>
 
-<h1>Counter : {count}</h1>
+<h1>{count.join(" + ")} = {total}</h1>
 
 <button onclick={increment}>Increment</button>
