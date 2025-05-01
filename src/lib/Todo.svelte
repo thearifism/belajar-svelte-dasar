ae1312e419b6bcb3c57de731f3f93a546ca94d43
krearive <arifblogger77@gmail.com>
2025-05-01 14:26:12 +0700

    Add dynamic data handling and emoji display for todos
    Replaced static todo list with reactive data binding using `data` array and `each` block, enabling dynamic updates. Added an emoji display based on the todo's `id` and a "Remove" button to delete the first item in the list. Improved the usability and interaction of the Todo app.

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,6 +1,18 @@
 <script>
   const { id, name, done } = $props();
+
+  const emojis = {
+    "1": "👀",
+    "2": "😭",
+    "3": "🫣",
+    "4": "👨‍💻",
+  };
+
+  const emoji = emojis[id];
+  console.log(emoji);
 </script>
+
+{emoji}
 
 {#if done}
   <strike> {id} - {name}</strike>
--- src/lib/TodoApp.svelte
+++ src/lib/TodoApp.svelte
@@ -1,10 +1,22 @@
 <script>
   import Todo from "./Todo.svelte";
+
+  const data = $state([
+    { id: 1, name: "Belajar HTML", done: true },
+    { id: 2, name: "Belajar CSS", done: false },
+    { id: 3, name: "Belajar JavaScript", done: true },
+    { id: 4, name: "Belajar NodeJS", done: false },
+  ]);
+
+  function remove() {
+    data.shift();
+  }
 </script>
 
+<button onclick={remove}>Remove</button>
+
 <ul>
-  <li><Todo id="1" name="Belajar HTML" done={true} /></li>
-  <li><Todo id="2" name="Belajar CSS" done={false} /></li>
-  <li><Todo id="3" name="Belajar JavaScript" done={true} /></li>
-  <li><Todo id="4" name="Belajar NodeJS" done={false} /></li>
+  {#each data as todo (todo.id)}
+    <li><Todo {...todo} /></li>
+  {/each}
 </ul>
