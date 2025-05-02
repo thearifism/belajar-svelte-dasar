c9a9af862856614fbd198104c1ab727f9891f184
krearive <arifblogger77@gmail.com>
2025-05-02 17:18:48 +0700

    Add mouse tracking feature with new Svelte component
    Introduced a `Mouse.svelte` component that tracks and displays cursor coordinates. Updated Vite config to include a new `mouse.html` entry point and added supporting files for functionality. This enhances the app with real-time mouse position updates.

---
+++ src/lib/Mouse.svelte
@@ -0,0 +1,16 @@
+<script>
+  const mouse = $state({
+    x: 0,
+    y: 0,
+  });
+
+  function handleMouseMove(event) {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+  }
+</script>
+
+<svelte:body onmousemove={handleMouseMove} />
+
+<h1>X : {mouse.x}</h1>
+<h1>Y : {mouse.y}</h1>
--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
         store_counter: 'store-counter.html',
         blog: 'blog.html',
         context_counter: 'context-counter.html',
+        mouse: 'mouse.html',
       },
     },
   },
