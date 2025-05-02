4699ee0b50f8188485710742f69075445c307a00
krearive <arifblogger77@gmail.com>
2025-05-02 17:11:39 +0700

    Fetch blog data from JSON and bind dynamically in Blog.svelte
    Replaced static blog content with dynamic data fetched from `blog.json`. The `onMount` lifecycle method initializes the blog state, allowing the title, content, and footer to update dynamically from the fetched JSON.

--- src/lib/Blog.svelte
+++ src/lib/Blog.svelte
@@ -1,20 +1,21 @@
 <script>
+  import { onMount } from "svelte";
   import Layout from "./Layout.svelte";
+
+  let blog = $state({
+    title: "",
+    content: "",
+    footer: "",
+  });
+
+  onMount(async () => {
+    const response = await fetch("/blog.json");
+    blog = await response.json();
+  });
 </script>
 
-<Layout title="Contoh Blog" footer="Copyright 2025">
+<Layout title={blog.title} footer={blog.footer}>
   {#snippet content()}
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat voluptatum
-      esse iusto libero, ipsum porro similique assumenda tempore deleniti ab
-      quasi animi odio expedita veniam voluptate velit, totam recusandae
-      impedit.
-    </p>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti rerum
-      commodi voluptatem modi consequuntur? Suscipit repudiandae et iusto alias
-      dicta officiis ea, provident veniam obcaecati quas. Culpa ad quis
-      similique.
-    </p>
+    {@html blog.content}
   {/snippet}
 </Layout>
